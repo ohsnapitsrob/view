7461c74e02c5a712952ff964ab64dcfade93af19
ohsnapitsrob <robholdersnaps@gmail.com>
2026-01-04 06:23:49 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,14 @@
+// config.js
 window.APP_CONFIG = {
   MAPTILER_KEY: "4iGDVzk2f6BFmiqgFyyU",
-  MAP_STYLE: "streets"
+  MAP_STYLE: "streets",
+
+  // Google Sheets tabs published as CSV:
+  // Put the direct CSV "publish to web" URLs for each tab here.
+  SHEETS: {
+    movies: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdUpTQ0w232gQ8tP2fEaGvdSe_q5SShKEWS7MOuku4VcCuTpg-rsTrTJ1ztpOyD6FMV98iZTGdzKt1/pub?gid=0&single=true&output=csv",
+    tv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdUpTQ0w232gQ8tP2fEaGvdSe_q5SShKEWS7MOuku4VcCuTpg-rsTrTJ1ztpOyD6FMV98iZTGdzKt1/pub?gid=1386447371&single=true&output=csv",
+    music_videos: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdUpTQ0w232gQ8tP2fEaGvdSe_q5SShKEWS7MOuku4VcCuTpg-rsTrTJ1ztpOyD6FMV98iZTGdzKt1/pub?gid=226124969&single=true&output=csv",
+    misc: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRdUpTQ0w232gQ8tP2fEaGvdSe_q5SShKEWS7MOuku4VcCuTpg-rsTrTJ1ztpOyD6FMV98iZTGdzKt1/pub?gid=82045520&single=true&output=csv"
+  }
 };
